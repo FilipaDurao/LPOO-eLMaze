bca71cc71eb8dd3abeba38b0ab7c22c0fd3a6e43
filipa <filipadurao@gmail.com>
2018-04-21 15:48:30 +0100

    Added Mobile icons and accelerometer reader

---
+++ eLMaze-Mobile/android/src/com/mygdx/elmaze/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.elmaze;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.elmaze.ELMaze;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ELMaze(), config);
+	}
+}
