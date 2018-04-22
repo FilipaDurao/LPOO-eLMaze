0b752ac7cfe9ddfe912a9dc617194d190f3c46f4
xRuiAlves <up201606746@fe.up.pt>
2018-04-22 23:18:20 +0100

    Removed screen auto-lock from application

--- eLMaze-Mobile/android/src/com/mygdx/elmaze/AndroidLauncher.java
+++ eLMaze-Mobile/android/src/com/mygdx/elmaze/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import android.view.WindowManager;
 import com.mygdx.elmaze.ELMaze;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -11,6 +12,11 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		// Remove auto-lock screen while application running
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
 		initialize(new ELMaze(), config);
 	}
 }
