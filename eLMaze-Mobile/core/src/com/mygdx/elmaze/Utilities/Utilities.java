63d89165bad0c93c122f72d2ba2dfc5195420bf3
xRuiAlves <up201606746@fe.up.pt>
2018-04-26 18:06:59 +0100

    Implemented Mobile IP Parser

---
+++ eLMaze-Mobile/core/src/com/mygdx/elmaze/Utilities/Utilities.java
@@ -0,0 +1,21 @@
+package com.mygdx.elmaze.Utilities;
+
+import java.util.LinkedList;
+
+public class Utilities {
+
+    public static String parse(LinkedList<Integer> rawIP) {
+        if (rawIP.size() != 8) {
+            return "";
+        }
+
+        String parsedIP = "";
+
+        for (int i=0 ; i<8 ; i+=2) {
+            parsedIP += (16*rawIP.get(i)+rawIP.get(i+1)) + (i != 6 ? "." : "");
+        }
+
+        return parsedIP;
+    }
+
+}
