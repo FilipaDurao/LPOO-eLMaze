2849e88afa8131678a205a8756e6da0e8b66b5ce
xRuiAlves <up201606746@fe.up.pt>
2018-05-15 17:07:07 +0100

    Created 'MessageToClient' class

---
+++ eLMaze-Mobile/core/src/com/mygdx/elmaze/networking/MessageToClient.java
@@ -0,0 +1,20 @@
+package com.mygdx.elmaze.networking;
+
+import java.io.Serializable;
+
+public class MessageToClient implements Serializable {
+
+    public enum CONTENT { GAME_START, GAME_FINISH };
+
+    private static final long serialVersionUID = 1L;
+    private final CONTENT content;
+
+    public MessageToClient(CONTENT content) {
+        this.content = content;
+    }
+
+    public CONTENT getContent() {
+        return content;
+    }
+
+}
