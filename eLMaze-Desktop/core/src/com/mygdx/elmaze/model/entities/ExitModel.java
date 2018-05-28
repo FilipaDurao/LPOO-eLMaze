bb82f2dc95644645eb23fbcde0ef3ac55e60dd81
filipa <filipadurao@gmail.com>
2018-05-28 14:42:45 +0100

    Added ExitModel and WallModel documentation

--- eLMaze-Desktop/core/src/com/mygdx/elmaze/model/entities/ExitModel.java
+++ eLMaze-Desktop/core/src/com/mygdx/elmaze/model/entities/ExitModel.java
@@ -4,15 +4,28 @@ public class ExitModel extends EntityModel {
 
 	private float radius;
 
+	/**
+	 * @param x The Exit's x position
+	 * @param y The Exit's y position
+	 * @param radius The Exit's radius
+	 */
 	public ExitModel(float x, float y, float radius) {
 		super(x, y);
 		this.radius = radius;
 	}
 
+	/**
+	 * @return Returns the radius of the Exit
+	 */
 	public float getRadius() {
 		return radius;
 	}
 
+	/**
+	 * @brief Sets the Exit's radius to the value passed as parameter
+	 * 
+	 * @param radius The value to set the Exit's radius
+	 */
 	public void setRadius(float radius) {
 		this.radius = radius;
 	}
--- eLMaze-Desktop/core/src/com/mygdx/elmaze/model/entities/WallModel.java
+++ eLMaze-Desktop/core/src/com/mygdx/elmaze/model/entities/WallModel.java
@@ -5,28 +5,56 @@ public class WallModel extends EntityModel {
 	private float width;
 	private float height;
 	
+	/**
+	 * @param x The Wall's x position
+	 * @param y The Wall's y position
+	 * @param width The Wall's width
+	 * @param height The Wall's height
+	 */
 	public WallModel(float x, float y, float width, float height) {
 		super(x, y);
 		this.width = width;
 		this.height = height;
 	}
 
+	/**
+	 * @return Returns the Wall's width
+	 */
 	public float getWidth() {
 		return width;
 	}
 
+	/**
+	 * @brief Sets the Wall's width to the value passed as parameter
+	 * 
+	 * @param width The value to set the Wall's width with
+	 */
 	public void setWidth(float width) {
 		this.width = width;
 	}
 
+	/**
+	 * @return Returns the Wall's height
+	 */
 	public float getHeight() {
 		return height;
 	}
 
+	/**
+	 * @brief Sets the Wall's height to the value passed as parameter
+	 * 
+	 * @param height The value to set the Wall's height with
+	 */
 	public void setHeight(float height) {
 		this.height = height;
 	}
 	
+	/**
+	 * @brief Resizes the Wall to the width and height passed as parameter
+	 * 
+	 * @param width The value to set the Wall's width with
+	 * @param height The value to set the Wall's height with
+	 */
 	public void resize(float width, float height) {
 		this.width = width;
 		this.height = height;
