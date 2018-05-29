4f87752f8bd6e2acb79f11a835762d5aa2085a8a
xRuiAlves <up201606746@fe.up.pt>
2018-05-29 21:11:15 +0100

    Added level related unit tests

--- eLMaze-Desktop/core/src/com/mygdx/elmaze/test/LevelTest.java
+++ eLMaze-Desktop/core/src/com/mygdx/elmaze/test/LevelTest.java
@@ -5,9 +5,13 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.mygdx.elmaze.model.levels.MPLevel1Model;
+import com.mygdx.elmaze.model.levels.MPLevel2Model;
+import com.mygdx.elmaze.model.levels.MPLevel3Model;
 import com.mygdx.elmaze.model.levels.SPLevel1Model;
 import com.mygdx.elmaze.model.levels.SPLevel2Model;
 import com.mygdx.elmaze.model.levels.SPLevel3Model;
+import com.mygdx.elmaze.model.levels.SPLevel4Model;
 
 public class LevelTest {
 
@@ -67,7 +71,7 @@ public class LevelTest {
 	
 	@Test 
 	public void SPLevelFourTest() {
-		SPLevel3Model level = new SPLevel3Model();
+		SPLevel4Model level = new SPLevel4Model();
 		
 		// Test Ball Player number
 		assertEquals(level.getBall().getPlayerNum(), 0);
@@ -85,17 +89,59 @@ public class LevelTest {
 	
 	@Test 
 	public void MPLevelOneTest() {
+		MPLevel1Model level = new MPLevel1Model();
 		
+		// Test Balls Player number
+		assertEquals(level.getBall1().getPlayerNum(), 0);
+		assertEquals(level.getBall2().getPlayerNum(), 1);
+		
+		// Test exit is existent
+		assertTrue(level.getExit() != null);
+		
+		// Test buttons and doors quantities
+		assertEquals(level.getDoors().size(), 2);
+		assertEquals(level.getButtons().size(), 2);
+		
+		// Test walls quantites
+		assertEquals(level.getWalls().size(), 7);
 	}
 	
 	@Test 
 	public void MPLevelTwoTest() {
+		MPLevel2Model level = new MPLevel2Model();
 		
+		// Test Balls Player number
+		assertEquals(level.getBall1().getPlayerNum(), 0);
+		assertEquals(level.getBall2().getPlayerNum(), 1);
+		
+		// Test exit is existent
+		assertTrue(level.getExit() != null);
+		
+		// Test buttons and doors quantities
+		assertEquals(level.getDoors().size(), 2);
+		assertEquals(level.getButtons().size(), 2);
+		
+		// Test walls quantites
+		assertEquals(level.getWalls().size(), 15);
 	}
 	
 	@Test 
 	public void MPLevelThreeTest() {
+		MPLevel3Model level = new MPLevel3Model();
 		
+		// Test Balls Player number
+		assertEquals(level.getBall1().getPlayerNum(), 0);
+		assertEquals(level.getBall2().getPlayerNum(), 1);
+		
+		// Test exit is existent
+		assertTrue(level.getExit() != null);
+		
+		// Test buttons and doors quantities
+		assertEquals(level.getDoors().size(), 1);
+		assertEquals(level.getButtons().size(), 4);
+		
+		// Test walls quantites
+		assertEquals(level.getWalls().size(), 16);
 	}
 
 }
