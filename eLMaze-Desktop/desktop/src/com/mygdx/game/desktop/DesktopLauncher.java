7c8f78ffc1d21b7a864bad89a673432c4fd8e594
xRuiAlves <up201606746@fe.up.pt>
2018-06-01 11:42:47 +0100

    Added fullscreen to desktop application

--- eLMaze-Desktop/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ eLMaze-Desktop/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,8 +7,12 @@ import com.mygdx.elmaze.ELMaze;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
 		config.width = 1024;
-		config.height = 768;
+	    config.height = 768;
+	    config.resizable = false;
+	    config.fullscreen = true;
+		
 		new LwjglApplication(new ELMaze(), config);
 	}
 }
